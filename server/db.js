d2c6665094d44d59554c99a455fa0d514bb7c6f5
Fabis1562 <106451155+Fabis1562@users.noreply.github.com>
2025-11-19 11:32:40 -0600

    fix: Update database password handling in connection configuration

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-
+console.log("La contraseña que estoy leyendo es:", process.env.DB_PASSWORD);
 const dbName = process.env.DB_NAME;
 
 const connectionConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: 'password123',
 };
 
 let connection;
